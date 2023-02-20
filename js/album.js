549685982d63be4a461c1fe7cecd25eccccd3bc9
Matilde Hadrup Mikkelsen <mati1980@kea.stud.dk>
2023-02-20 14:02:38 +0100

    album

--- js/album.js
+++ js/album.js
@@ -2,30 +2,29 @@
 const urlParams = new URLSearchParams(window.location.search);
 console.log("urlParams", urlParams);
 //find id
-const id = urlParams.get("id");
-console.log("id", id);
+const id = urlParams.get("_id");
+console.log("_id", id);
 
-const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
+const url = `https://musicgallery-27fd.restdb.io/rest/albums/${id}`;
 
-const endpoint = `https://kea-alt-del.dk/t7/api/products/${id}`;
+const options = {
+  headers: {
+    "x-apikey": "63efb163478852088da68404",
+  },
+};
 
-function hentData() {
-  fetch(endpoint)
-    .then((respons) => respons.json())
-    .then(visProdukt);
+async function hentData() {
+  const respons = await fetch(url, options);
+  const json = await respons.json();
+  visAlbum();
 }
 
-function visProdukt(produkt) {
-  console.log(produkt);
-  document.querySelector(".model").textContent = produkt.productdisplayname;
-  document.querySelector(".info").querySelector(".model").textContent = produkt.productdisplayname;
-  document.querySelector("#variantname").textContent = produkt.variantname;
-  document.querySelector("#basecolour").textContent = produkt.basecolour;
-  document.querySelector("#id").textContent = produkt.id;
-  document.querySelector(".brandname").textContent = produkt.brandname;
-  document.querySelector(".info").querySelector(".brandname").textContent = produkt.brandname;
-  document.querySelector("#brandbio").textContent = produkt.brandbio;
-  document.querySelector("img").src = imagePath;
+function visAlbum(album) {
+  console.log(album);
+  document.querySelector("img").src = "" + album.albumcover;
+  document.querySelector("h1").textContent = album.album;
+  document.querySelector("h3").textContent = album.artist + " / " + album.year;
+  document.querySelector(".sange").innerHTML = album.songs;
 }
 
 hentData();
