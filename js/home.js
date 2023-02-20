cce979018ad8db7ee495af2663a8762a691066e9
Kia Meggele <kia1262@stud.kea.dk>
2023-02-20 12:19:39 +0100

    yes

--- js/home.js
+++ js/home.js
@@ -1,25 +1,32 @@
-const url = "https://musicgallery-27fd.restdb.io/rest/albums";
+const urlParams = new URLSearchParams(window.location.search);
+const album = urlParams.get("album");
+
+const url = "https://musicgallery-27fd.restdb.io/rest/albums?max=8";
+
 const options = {
   headers: {
     "x-apikey": "63efb163478852088da68404",
   },
 };
+
 async function hentData() {
   const resspons = await fetch(url, options);
   const json = await resspons.json();
   vis(json);
 }
 const main = document.querySelector("main");
-const template = document.querySelector("template").content;
+const template = document.querySelector("#template_con").content;
 
 function vis(json) {
   console.log(json);
   json.forEach((album) => {
     const klon = template.cloneNode(true);
-    klon.querySelector("img").src = "billeder/" + album.billede;
-    klon.querySelector("h2").textContent = album.titel;
+    klon.querySelector;
+    klon.querySelector("h2").textContent = album.album;
     klon.querySelector("h3").textContent = album.artist;
+
     main.appendChild(klon);
   });
 }
+
 hentData();
